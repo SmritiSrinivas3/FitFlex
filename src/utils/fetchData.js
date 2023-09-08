94931e18dcba137721329359757fda4eb5855aca
SmritiSrinivas3 <smritisrinivasneelamana@gmail.com>
2023-09-08 10:05:20 +0530

    fixed

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,22 +1,23 @@
 
 
-
 export const exOptions =  {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '4985fe7433mshb47ed5cb09d2335p15614djsn3b5d182a56c5',
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-    }
-  };
-  
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'ab6bb00cb9msh12c1784a16fb37bp1a6cf8jsn4477b74f8938',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+};
 
-  export const youtubeOptions = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '4985fe7433mshb47ed5cb09d2335p15614djsn3b5d182a56c5',
-      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
-    },
-  };
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'ab6bb00cb9msh12c1784a16fb37bp1a6cf8jsn4477b74f8938',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  },
+};
+
+
   
 
 
